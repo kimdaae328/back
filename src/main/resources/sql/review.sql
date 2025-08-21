d790de9fedb6ef6b9c59b4ff995900120e18a4b0
ccocco55 <ccocco55@gmail.com>
2025-08-21 17:35:56 +0900

    #6

--- src/main/resources/sql/review.sql
+++ src/main/resources/sql/review.sql
@@ -19,28 +19,5 @@ create table tbl_review(
 
 select * from tbl_review;
 
-INSERT INTO tbl_review (review_content, review_status, member_id, request_id, product_id) VALUES
-('리뷰 내용 1', 'active', 1, 49, 2),
-('리뷰 내용 2', 'active', 2, 50, 2),
-('리뷰 내용 3', 'active', 3, 51, 2),
-('리뷰 내용 4', 'active', 4, 52, 2),
-('리뷰 내용 5', 'active', 5, 53, 2),
-('리뷰 내용 6', 'active', 6, 54, 2),
-('리뷰 내용 7', 'active', 7, 49, 2),
-('리뷰 내용 8', 'active', 1, 50, 2),
-('리뷰 내용 9', 'active', 2, 51, 2),
-('리뷰 내용 10', 'active', 3, 52, 2),
-('리뷰 내용 11', 'active', 4, 53, 2),
-('리뷰 내용 12', 'active', 5, 54, 2),
-('리뷰 내용 13', 'active', 6, 49, 2),
-('리뷰 내용 14', 'active', 7, 50, 2),
-('리뷰 내용 15', 'active', 1, 51, 2),
-('리뷰 내용 16', 'active', 2, 52, 2),
-('리뷰 내용 17', 'active', 3, 53, 2),
-('리뷰 내용 18', 'active', 4, 54, 2),
-('리뷰 내용 19', 'active', 5, 49, 2),
-('리뷰 내용 20', 'active', 6, 50, 2);
-
-
 
 drop table tbl_review;
