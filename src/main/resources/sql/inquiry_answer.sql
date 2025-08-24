8abee3cefeac5270e68c99c47c42b769aabfab00
kimdaae <kimdaae328@gmail.com>
2025-08-25 04:22:08 +0900

    sql 정리 #40

--- src/main/resources/sql/inquiry_answer.sql
+++ src/main/resources/sql/inquiry_answer.sql
@@ -14,16 +14,3 @@ create table tbl_inquiry_answer(
 
 select * from tbl_inquiry_answer;
 
-select i.id, i.inquiry_category, i.inquiry_title, i.inquiry_content, i.inquiry_image, i.member_id, i.created_date as inquiryCreatedDate, i.updated_date as inquiryUpdatedDate,
-       m.member_name, ia.inquiry_id, ia.inquiry_answer_content, ia.created_date as answerCreatedDate, ia.updated_date as answerUpdatedDate
-from tbl_inquiry i
-         left join tbl_inquiry_answer ia
-                   on ia.inquiry_id = i.id
-         join tbl_member m
-              on m.id = i.member_id
-where m.member_status = 'active' and m.member_role = 'seller'
-  and ia.inquiry_answer_content is not null
-order by i.id desc
-
-drop table tbl_inquiry_answer;
-
