1e8e5f659bc9d9ea3e15d6d0fbf913efaacfaa34
kimdaae <kimdaae328@gmail.com>
2025-08-29 11:11:14 +0900

    회원 목록 버그 내용없을때 제외 완료 #72

--- src/main/java/com/example/youeatieat/controller/AdminController.java
+++ src/main/java/com/example/youeatieat/controller/AdminController.java
@@ -8,29 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/**")
 public class AdminController {
 
-//    메인
-//    @GetMapping
-//    public String adminPage(){
-//        return "admin/main/main";
-//    }
-
 //    회원목록
     @GetMapping
     public String adminPage(){
         return "admin/customer/list";
     }
 
-//    판매자목록
-//    @GetMapping
-//    public String adminPage(){
-//        return "admin/seller/list";
-//    }
-
-//    문의목록
-//    @GetMapping
-//    public String adminPage(){
-//        return "admin/inquiry/customer-inquiry";
-//    }
 
 }
 
